adcf303b9035222c481e52931eabcc30d520ac00
PhuongHa <tranphuongha0903@gmail.com>
2021-09-17 10:28:58 +0700

    build gradle

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -7,17 +7,24 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.helloandroid.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 
     public void onClick(View view) {
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.paris);
+//        ImageView imageView = findViewById(R.id.imageView);
+//        imageView.setImageResource(R.drawable.paris);
+        binding.imageView.setImageResource(R.drawable.paris);
         Toast.makeText(this, "You have clicked!", Toast.LENGTH_LONG).show();
     }
 }
